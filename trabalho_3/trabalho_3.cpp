ac9737a33c4b387c9443e7a112b65d8b59b1978f
Guilherme Barbosa <guilhermecirineu@hotmail.com>
2025-10-19 17:10:19 -0300

    Implementação da esfera e dois planos, incluindo o shadow ray Co-authored-by: Isaac_Mosiah <IsaacMosiah@users.noreply.github.com>

--- trabalho_3/trabalho_3.cpp
+++ trabalho_3/trabalho_3.cpp
@@ -25,9 +25,9 @@ float prod_escalar(vector<float>& v, vector<float>& p){
     return (v[0] * p[0]) + (v[1] * p[1]) + (v[2] * p[2]);
 }
 
-/*vector<float> prod_vetorial(vector<float>& v, vector<float>& p){
+vector<float> prod_vetorial(vector<float>& v, vector<float>& p){
     
-}*/
+}
 
 vector<float> mult_escalar(vector<float>& v, float x) {
     vector<float> q;
@@ -214,39 +214,149 @@ int main() {
             } else {
                 /* ============= VERIFICAÇÃO PARA PLANO CHÃO ============= */
                 vector<float> w_p_chao = subt(olhoPintor, P_pi_c);
-                float wpcnc = prod_escalar(w_p_chao, n_bar_c);
+                float wpcnc = - prod_escalar(w_p_chao, n_bar_c);
                 float drnc = prod_escalar(dr_u, n_bar_c);
                 float ti_p_c = wpcnc / drnc;
 
-                if (ti_p_c >= 0) {
-                    fp.put(100);
-                    fp.put(100);
-                    fp.put(255);
-                } else {
+                /* ============= VERIFICAÇÃO PARA PLANO FUNDO ============= */
+                vector<float> w_p_fundo = subt(olhoPintor, P_pi_f);
+                float wpfnf = - prod_escalar(w_p_fundo, n_bar_f);
+                float drnf = prod_escalar(dr_u, n_bar_f);
+                float ti_p_f = wpfnf / drnf;
 
-                    /* ============= VERIFICAÇÃO PARA PLANO FUNDO ============= */
-                    // Baseado na Equação do Plano (Pj - Ppi) n = 0, criamos um vetor w_p_fundo que corresponde a (Olho Pintor - Ppi)
-                    vector<float> w_p_fundo = subt(olhoPintor, P_pi_f);
+                if (ti_p_c <= ti_p_f && ti_p_c > 0) {
+                    // Calcula P(t) de acordo com a equação do Ray
+                    vector<float> tdr = mult_escalar(dr_u, ti_p_c);
+                    vector<float> Pi = soma(olhoPintor, tdr);
 
-                    // Com isso, calculamos o ti, se for maior ou igual a 0, então o raio intercepta o plano.
-                    float wpfnf = - prod_escalar(w_p_fundo, n_bar_f);
-                    float drnf = prod_escalar(dr_u, n_bar_f);
-                    float ti_p_f = wpfnf / drnf;
+                    // Calcula l
+                    vector<float> pfPI = subt(fonte_p_coord, Pi);
+                    float pfPI_norma = norma(pfPI);
+                    vector<float> l = div_escalar(pfPI, pfPI_norma);
 
-                    if (ti_p_f >= 0) {
-                        // o raio acerta o plano. Vamos calcular Id, Ie e Iamb para o plano de fundo
-                        fp.put(210);
-                        fp.put(210);
-                        fp.put(210);
-                    } else {
-                        fp.put(bgColor[0]);
-                        fp.put(bgColor[1]);
-                        fp.put(bgColor[2]);
+                    // P(s) = Pi + s*l
+                    vector<float> s_esf = subt(Pi, centroEsfera);
+
+                    // Com o w podemos calcular os componentes a, b e c da nossa equação do segundo grau
+                    float aDelta = prod_escalar(l, l);
+
+                    float bDelta = 2 * prod_escalar(s_esf, l);
+
+                    float cDelta = prod_escalar(s_esf, s_esf) - pow(rEsfera, 2) ;
+
+                    float delta = pow(bDelta,2) - (4 * aDelta * cDelta);
+
+                    if (delta >= 0) {
+                        l = criarVetor(0.0f, 0.0f, 0.0f);
                     }
+
+                    // Calcula x
+                    vector<float> x = {-dr_u[0], -dr_u[1], -dr_u[2]};
+
+                    // Calcula r
+                    float ln = prod_escalar(l, n_bar_c);
+                    vector<float> r = {(2*ln*n_bar_c[0]) - l[0], (2*ln*n_bar_c[1]) - l[1], (2 * ln*n_bar_c[2]) - l[2]};
+
+                    // Calcular Id e Ie e Iamb
+                    float ln_limitado = max(0.0f, ln);
+                    vector<float> Ifkd = arroba(fonte_p_int, Kd_c);
+                    vector<float> Id = mult_escalar(Ifkd, ln_limitado);
+
+                    float rx = max(0.0f, prod_escalar(r, x));
+                    float rxm = pow(rx, m_c);
+                    vector<float> Ifke = arroba(fonte_p_int, Ke_c);
+                    vector<float> Ie = mult_escalar(Ifke, rxm);
+
+                    vector<float> Iamb = arroba(I_A, Kd_c);
+
+                    vector<float> I_E_c = {Ie[0] + Id[0] + Iamb[0], Ie[1] + Id[1] + Iamb[1], Ie[2] + Id[2] + Iamb[2]};
+                    
+                    // Imprimir as cores no arquivo
+                    if (I_E_c[0] > 1.0f){
+                        fp.put(255);
+                    } else{
+                        fp.put(I_E_c[0] * 255);
+                    }
+                    if (I_E_c[1] > 1.0f){
+                        fp.put(255);
+                    } else{
+                        fp.put(I_E_c[1] * 255);
+                    }
+                    if (I_E_c[2] > 1.0f){
+                        fp.put(255);
+                    } else{
+                        fp.put(I_E_c[2] * 255);
+                    }
+
+                } else if (ti_p_f > 0) {
+                    // Calcula P(t) de acordo com a equação do Ray
+                    vector<float> tdr = mult_escalar(dr_u, ti_p_f);
+                    vector<float> Pi = soma(olhoPintor, tdr);
+
+                    // Calcula l
+                    vector<float> pfPI = subt(fonte_p_coord, Pi);
+                    float pfPI_norma = norma(pfPI);
+                    vector<float> l = div_escalar(pfPI, pfPI_norma);
+
+                    // P(s) = Pi + s*l
+                    vector<float> s_esf = subt(Pi, centroEsfera);
+
+                    // Com o w podemos calcular os componentes a, b e c da nossa equação do segundo grau
+                    float aDelta = prod_escalar(l, l);
+
+                    float bDelta = 2 * prod_escalar(s_esf, l);
+
+                    float cDelta = prod_escalar(s_esf, s_esf) - pow(rEsfera, 2) ;
+
+                    float delta = pow(bDelta,2) - (4 * aDelta * cDelta);
+
+                    if (delta >= 0) {
+                        l = criarVetor(0.0f, 0.0f, 0.0f);
+                    }
+
+                    // Calcula x
+                    vector<float> x = {-dr_u[0], -dr_u[1], -dr_u[2]};
+
+                    // Calcula r
+                    float ln = prod_escalar(l, n_bar_f);
+                    vector<float> r = {(2*ln*n_bar_f[0]) - l[0], (2*ln*n_bar_f[1]) - l[1], (2 * ln*n_bar_c[2]) - l[2]};
+
+                    // Calcular Id e Ie e Iamb
+                    float ln_limitado = max(0.0f, ln);
+                    vector<float> Ifkd = arroba(fonte_p_int, Kd_f);
+                    vector<float> Id = mult_escalar(Ifkd, ln_limitado);
+
+                    float rx = max(0.0f, prod_escalar(r, x));
+                    float rxm = pow(rx, m_f);
+                    vector<float> Ifke = arroba(fonte_p_int, Ke_f);
+                    vector<float> Ie = mult_escalar(Ifke, rxm);
+
+                    vector<float> Iamb = arroba(I_A, Kd_f);
+
+                    vector<float> I_E_f = {Ie[0] + Id[0] + Iamb[0], Ie[1] + Id[1] + Iamb[1], Ie[2] + Id[2] + Iamb[2]};
+
+                    if (I_E_f[0] > 1.0f){
+                        fp.put(255);
+                    } else{
+                        fp.put(I_E_f[0] * 255);
+                    }
+                    if (I_E_f[1] > 1.0f){
+                        fp.put(255);
+                    } else{
+                        fp.put(I_E_f[1] * 255);
+                    }
+                    if (I_E_f[2] > 1.0f){
+                        fp.put(255);
+                    } else{
+                        fp.put(I_E_f[2] * 255);
+                    }
+                        
+                } else {
+                    fp.put(bgColor[0]);
+                    fp.put(bgColor[1]);
+                    fp.put(bgColor[2]);
                 }
             }
-            /*
-            }*/
         }
     }
 
